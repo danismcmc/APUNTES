77df4a77158197b86b6014e7e10cc8696aa16b1a
danismc <danismc0201@gmail.com>
2025-05-06 13:05:31 +0200

    Dani Sanchez - Segundo commit

---
+++ src/es/mdc/FUtil.java
@@ -0,0 +1,11 @@
+package es.mdc; 
+import java.io.File; 
+public class FUtil { 
+public static boolean existe(String filename) {  
+File file = new File(filename); 
+if (file.exists()) { 
+return true; 
+} 
+return false; 
+} 
+}
