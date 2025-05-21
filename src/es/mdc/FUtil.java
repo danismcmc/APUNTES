0bcbc05a36ba5aeb623866460645c48baa994737
danismcmc <116626302+danismcmc@users.noreply.github.com>
2025-05-21 21:19:21 +0200

    Update FUtil.java

--- src/es/mdc/FUtil.java
+++ src/es/mdc/FUtil.java
@@ -1,11 +1,26 @@
 package es.mdc; 
 import java.io.File; 
-public class FUtil { 
-public static boolean existe(String filename) {  
-File file = new File(filename); 
-if (file.exists()) { 
-return true; 
-} 
-return false; 
-} 
+
+
+/**
+ * Verifica si un archivo existe a partir del nombre de archivo proporcionado.
+ *
+ * @param filename el nombre del archivo a verificar. No debe ser null ni estar vacío.
+ * @return true si el archivo existe, false en caso contrario.
+ * @throws IllegalArgumentException si el nombre del archivo es null, vacío o contiene solo espacios en blanco.
+ * @author Dani Sanchez
+ */
+
+public class FUtil {
+	
+	    public static boolean existe(String filename) throws IllegalArgumentException {
+	    
+	        if (filename == null || filename.trim().isEmpty()) {
+	            throw new IllegalArgumentException("El nombre del archivo no puede estar vacío o contener solo espacios");
+	        }
+
+	        File file = new File(filename);
+	        return file.exists();
+
 }
+}	
